090f56304b64bce11a61b9c6dfd293abf58656be
Marc Jones <marc.jones@crick.ac.uk>
2021-05-21 15:10:06 +0100

    Added full functional test

---
+++ tests/functional/test_full_run.py
@@ -0,0 +1,13 @@
+import clip
+import os
+
+def test_full_run(rootdir, monkeypatch, tmp_path):
+    monkeypatch.setattr("sys.argv", ['clip.py',
+        '-i', os.path.join(rootdir, 'tests', 'data', 'crosslinkcounts.bed'),
+        '-a', os.path.join(rootdir, 'tests', 'data', 'annot.gff'),
+        '-o', os.path.join(tmp_path, 'clippy')])
+    clip.main()
+    for file_name in os.listdir(tmp_path):
+        test_lines = open(os.path.join(tmp_path, file_name)).readlines()
+        ref_lines = open(os.path.join(rootdir, 'tests', 'data', 'output', file_name)).readlines()
+        assert(test_lines == ref_lines)
