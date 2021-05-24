84ab0d02cb51597b3e3d8e421342c382c75c3b45
Marc Jones <marc.jones@crick.ac.uk>
2021-05-24 14:51:06 +0100

    Added profiling code

--- README.md
+++ README.md
@@ -79,6 +79,12 @@ To run the automated testing, use:
 pytest --cov=clip
 ```
 
+To run the profiling code:
+
+```
+pytest -k profiling --profile-svg
+```
+
 ### Concept
 Using the annotation provided, crosslinks over each gene are smoothed using a rolling mean. The window can be decided by the user. For each gene the mean of the smoothed signal is taken (red line) and the mean + (standard deviation * adjustment factor) (green line) is taken. The mean is used to define the minimum height of a peak. The mean + (standard deviation * adjustment factor) is taken to define the minimum prominence of a peak. Please see [here](https://en.wikipedia.org/wiki/Topographic_prominence#:~:text=The%20prominence%20of%20a%20peak,or%20key%20saddle%2C%20or%20linking) for the definition of topographical prominence. Essentially this parameter allows that we do not call many shallow peaks in a region where there is a clearly more prominent peak. 
 
--- tests/functional/test_full_run.py
+++ tests/functional/test_full_run.py
@@ -1,5 +1,7 @@
 import clip
 import os
+import pybedtools
+import pandas as pd
 
 def test_full_run(rootdir, monkeypatch, tmp_path):
     monkeypatch.setattr("sys.argv", ['clip.py',
@@ -12,3 +14,24 @@ def test_full_run(rootdir, monkeypatch, tmp_path):
         test_lines = open(os.path.join(tmp_path, file_name)).readlines()
         ref_lines = open(os.path.join(rootdir, 'tests', 'data', 'output', file_name)).readlines()
         assert(test_lines == ref_lines)
+
+def test_getThePeaks_profiling(rootdir):
+    xlinks = pybedtools.BedTool(os.path.join(rootdir, 'tests', 'data', 'crosslinkcounts.bed'))
+    annot = pd.read_table(
+        os.path.join(rootdir, 'tests', 'data', 'annot.gff'),
+        header=None,
+        names=["chrom","source","feature_type","start","end","score","strand","frame","attributes"],
+        comment='#')
+    annot_gene = annot[annot.feature_type=="gene"]
+    ang = pybedtools.BedTool.from_dataframe(annot_gene).sort()
+    goverlaps = xlinks \
+        .intersect(ang, s=True, wo=True) \
+        .to_dataframe(names=['chrom', 'start', 'end', 'name', 'score', 'strand', 'chrom2', 'source',
+            'feature', 'gene_start', 'gene_stop', 'nothing', 'strand2', 'nothing2', 'gene_name', 'interval']) \
+        .drop(['name', 'chrom2', 'nothing', 'nothing2', 'interval', 'strand2', 'source', 'feature'], axis=1)
+    arguments_list = [
+        (pd.DataFrame(y), 50, 1, 0.8, 5)
+        for x, y in goverlaps.groupby('gene_name', as_index=False)
+    ]
+    output = [clip.getThePeaks(*args) for args in arguments_list[:1000]]
+    assert(len(output) == 1000)
