728fbafac5f96e09bead53b28a1a903879e03000
Marc Jones <marc.jones@crick.ac.uk>
2021-09-23 17:39:57 +0100

    Added tests for alt_features

--- tests/functional/test_alt.py
+++ tests/functional/test_alt.py
@@ -22,3 +22,45 @@ def test_alt_features_full_run(rootdir, monkeypatch, tmp_path):
         ],
     )
     clip.main()
+
+
+def test_no_alt_features_cep295(rootdir, monkeypatch, tmp_path):
+    monkeypatch.setattr(
+        "sys.argv",
+        [
+            "clip.py",
+            "-i",
+            os.path.join(rootdir, "tests", "data", "cep295.bed"),
+            "-a",
+            os.path.join(rootdir, "tests", "data", "gencode.v38.cep295.gtf"),
+            "-o",
+            os.path.join(tmp_path, "cep295"),
+            "-g",
+            os.path.join(rootdir, "tests", "data", "human_genome.fa.fai"),
+        ],
+    )
+    clip.main()
+    monkeypatch.setattr(
+        "sys.argv",
+        [
+            "clip.py",
+            "-i",
+            os.path.join(rootdir, "tests", "data", "cep295.bed"),
+            "-a",
+            os.path.join(rootdir, "tests", "data", "gencode.v38.cep295.gtf"),
+            "-o",
+            os.path.join(tmp_path, "cep295_scarna"),
+            "-g",
+            os.path.join(rootdir, "tests", "data", "human_genome.fa.fai"),
+            "-alt",
+            "scaRNA-gene_name-SCARNA[0-9]+",
+        ],
+    )
+    clip.main()
+    for file_name in os.listdir(tmp_path):
+        test_lines = open(os.path.join(tmp_path, file_name)).readlines()
+        ref_lines = open(
+            os.path.join(rootdir, "tests", "data", "output", file_name)
+        ).readlines()
+        assert test_lines == ref_lines
+
